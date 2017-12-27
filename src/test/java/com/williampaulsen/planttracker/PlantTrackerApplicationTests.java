8b1a0fc0ce3fc4f59a5071735ccd668ca9cca5e3
William Paulsen <bpswim90@gmail.com>
2017-12-27 11:55:00 -0600

    Initial commit.

---
+++ src/test/java/com/williampaulsen/planttracker/PlantTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.williampaulsen.planttracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
