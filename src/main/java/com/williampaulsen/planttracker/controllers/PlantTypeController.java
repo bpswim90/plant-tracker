0fbae0e878ebd013eeca280c533219dd151eb7df
William Paulsen <bpswim90@gmail.com>
2017-12-28 14:05:12 -0600

    Added controller for processing addPlantType form.

--- src/main/java/com/williampaulsen/planttracker/controllers/PlantTypeController.java
+++ src/main/java/com/williampaulsen/planttracker/controllers/PlantTypeController.java
@@ -4,25 +4,30 @@ import com.williampaulsen.planttracker.models.LightPreference;
 import com.williampaulsen.planttracker.models.PlantType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping(value="plant/type")
 public class PlantTypeController {
+
+    ArrayList<PlantType> plantTypes = new ArrayList<>();
 
     @RequestMapping(value = "")
     public String index(Model model) {
         PlantType aloe = new PlantType("Aloe",21);
         aloe.setLightPreference(LightPreference.PART_SHADE);
 
-        model.addAttribute("plantType",aloe);
-        model.addAttribute("title","Plant Profile: " + aloe.getName());
+        model.addAttribute("plantTypes",plantTypes);
+        model.addAttribute("title","Plants");
 
         return "plantType/index";
     }
 
-    @RequestMapping(value="/add", method= RequestMethod.GET)
+    @RequestMapping(value="/add", method=RequestMethod.GET)
     public String addPlantType(Model model) {
         model.addAttribute("title", "Add New Plant Type");
         model.addAttribute("plantType", new PlantType());
@@ -31,5 +36,10 @@ public class PlantTypeController {
         return "plantType/add";
     }
 
-    //TODO: Add controller to process add form and redirect to the new plant type profile
+    @RequestMapping(value="/add", method=RequestMethod.POST)
+    public String addPlantType(Model model, @ModelAttribute PlantType newPlantType) {
+        plantTypes.add(newPlantType);
+
+        return "redirect:";
+    }
 }
