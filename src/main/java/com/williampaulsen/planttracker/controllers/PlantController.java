fd783e0634921e8ddf4d7e691fceabf4d92c6207
William Paulsen <bpswim90@gmail.com>
2018-01-03 19:39:29 -0600

    Created new selectType controller to ask user to select a plant type before full add form, and associated view.

--- src/main/java/com/williampaulsen/planttracker/controllers/PlantController.java
+++ src/main/java/com/williampaulsen/planttracker/controllers/PlantController.java
@@ -4,6 +4,7 @@ import com.williampaulsen.planttracker.models.LightPreference;
 import com.williampaulsen.planttracker.models.Plant;
 import com.williampaulsen.planttracker.models.PlantType;
 import com.williampaulsen.planttracker.models.data.PlantDao;
+import com.williampaulsen.planttracker.models.data.PlantTypeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value="plant")
 public class PlantController {
+
+    @Autowired
+    private PlantTypeDao plantTypeDao;
 
     @Autowired
     private PlantDao plantDao;
@@ -24,6 +28,16 @@ public class PlantController {
         model.addAttribute("title","My Plants");
 
         return "plant/index";
+    }
+
+    //This view will ask the user to select the desired plant type or create a new type.
+    @RequestMapping(value="add")
+    public String selectType(Model model) {
+
+        model.addAttribute("title","Add New Plant");
+        model.addAttribute("plantTypes",plantTypeDao.findAll());
+
+        return "plant/selectType";
     }
 
     //TODO: Write a controller to display an add form for a new plant. 
